f645056d7f5b952e4b20b9c842de197019caa98d
José Rodolpho <j.rodolpho29@hotmail.com>
2022-05-14 13:47:48 -0300

    update: cart style

--- src/templates/CartScreen/index.jsx
+++ src/templates/CartScreen/index.jsx
@@ -1,14 +1,18 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import styled from "styled-components"
-
+import {AiOutlineShoppingCart, AiFillMinusCircle} from "react-icons/ai"
+import {BsFillPlusCircleFill} from "react-icons/bs"
+import {IoChevronBack} from "react-icons/io5"
 
 export default function Cart() {
   const [products, setProducts] = useState([])
   const [productInfo, setProductInfo] = useState([])
-  const [change, setChange] = useState(0)
+  const [update, setUpdate] = useState(true)
+  const [total, setTotal] = useState(0)
 
   useEffect(() =>{
+    if(update){
     axios
     .get("http://localhost:5000/cart",{
       headers: {
@@ -16,9 +20,18 @@ export default function Cart() {
       }
     })
     .then(response => {
-      setProducts(response.data)
+      setProducts(() => response.data)
     })
-  },[change])
+    .catch((e) => {
+      console.log(e)
+    })
+    setUpdate(false)
+  }
+  else{
+    console.log("aa")
+  }
+
+  },[update])
 
   useEffect(() => {
     if(products.length === 0) return
@@ -29,6 +42,7 @@ export default function Cart() {
         }
       })
       promise.then((response) => {
+        response.data.quantity = product.quantity
         setProductInfo((prev) => [response.data, ...prev])
       })
       promise.catch((e) => {
@@ -37,95 +51,181 @@ export default function Cart() {
     }
   },[products])
 
+  useEffect(() => {
+    setTotal(0)
+    productInfo.forEach((product) => {
+      const subTotal = parseFloat(product.price) * parseFloat(product.quantity)
+      console.log(productInfo)
+      setTotal((prev) => parseFloat(prev) + parseFloat(subTotal))
+    })
+  },[productInfo])
+
   return (
-    <div>
-      <Title>Meu carrinho</Title>
-      <div>
-        {productInfo.map((data) => 
-          <Product>
+    <CartDiv>
+      <Header>
+        <IoChevronBack/>
+        <Title> Meu carrinho  </Title>
+        <AiOutlineShoppingCart/>
+      </Header>
+      <ProductList>
+        {productInfo.map((data, index) => 
+          <Product key={index}>
             <img src={data.image} alt="product"></img>
             <Information>
-              <p>{data.name}</p>
+              <h1>{data.name}</h1>
               <p>R$ {data.price}</p>
             </Information>
             <Buttons>
-              <button onClick={() => deleteProduct(data._id)}>-</button>
-              <p>1</p>
-              <button onClick={() => addProduct(data._id)}>+</button>
+              <h1 onClick={() => DeleteProduct(data._id)}><AiFillMinusCircle/></h1>
+              <p>{data.quantity}</p>
+              <h1 onClick={() => AddProduct(data._id)}><BsFillPlusCircleFill/></h1>
             </Buttons>
           </Product>
         )}
-      </div>
-    </div>
+      </ProductList>
+      <Checkout>
+        <Total>
+          <p>Total: </p>
+          <p>R$ {parseFloat(total).toFixed(2)}</p>
+        </Total>
+          <button>Finalizar Compra</button>
+      </Checkout>
+    </CartDiv>
   )
 
-  function deleteProduct(id){
-    console.log(id)
-    axios
-    .delete(`http://localhost:5000/cart/${id}`)
-    .then((response) => {
-      console.log(response.data)
-      setChange(((prev) => 1 - prev))
-      setProductInfo([])
-    })
-    .catch((e) =>{
-      console.log(e)
-    })
+  function DeleteProduct(id){
+    axios.delete(`http://localhost:5000/cart/${id}`)
+    setProducts(() => [])
+    setProductInfo(() => [])
+    setUpdate(() => true)
   }
 
-  function addProduct(id){
-    console.log(id)
-    axios
-    .post(`http://localhost:5000/cart/${id}`)
-    .then((response) => {
-      setChange(((prev) => 1 + prev))
-      setProductInfo([])
-      console.log(response.data)
-    })
-    .catch((e) =>{
-      console.log(e)
-    })
-  }
+  function AddProduct(id){
+    axios.post(`http://localhost:5000/cart/${id}`,)
+    setProducts(() => [])
+    setProductInfo(() => [])
+    setUpdate(() => true)
+    }
 
   
 }
 
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 10px;
+  color: #015584;
+  font-size: 30px;
+`
+
+const CartDiv = styled.div`
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`
+ 
 const Title = styled.h1`
   margin: 30px 0;
   text-align: center;
+  font-weight: 700;
+  font-size: 25px;
+  color: #015584 ;
+`
+
+const ProductList = styled.div`
+  padding-bottom: 25%;
 `
 
 const Product = styled.div`
   display: flex;
-  width: 85%;
+  justify-content: space-between;
+  width: 85vw;
+  height: 200px;
   margin: 20px auto;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  color: #05A0F8;
+
+  h1{
+    margin-top: 10px;
+  }
   
+  p{
+    font-weight: 700;
+    margin: 10px 0;
+  }
+
   img{
     width: 90px;
-    height: 90%;
-    margin: auto;
+    height: 50%;
+    border-radius: 5px;
+    margin: auto 10px;
   }
 `
 
 const Information = styled.div`
   width: 50%;
   margin: auto 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
 const Buttons = styled.div`
   display: flex;
   height: 20%;
-  margin: auto;
+  margin: auto 10px;
   justify-content: center;
   align-items: center;
 
-  button{
-    border-radius: 100px;
-    width: 25%;
-    border: 0;
-    background-color: #FFAD32
+  h1{
+    color: #015584;
+    font-size: 20px;
+    margin: 0;
   }
+
   p{
     margin: 0 10px;
+  }
+`
+
+const Total = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  font-size: 20px;
+
+
+  p{
+    font-weight: 700;
+    color: #ffffff;
+  }
+`
+
+const Checkout = styled.div`
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #FFAD32 ;
+  
+  button{
+   width: 85%;
+   height: 40px;
+   border-radius: 5px;
+   margin-bottom: 10px;
+   background-color: #05A0F8;
+   color: #ffffff;
+   border: 0;
+   font-size: 20px;
+   font-weight: 700;
   }
 `
